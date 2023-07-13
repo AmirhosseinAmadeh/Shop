29b76f12f3262636965015ae8382f8a4ce39f705
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-07-13 13:48:42 +0330

    Interfaces added

--- Interface/Product.java
+++ Interface/Product.java
@@ -3,11 +3,14 @@ package Interface;
 import java.util.Date;
 
 public interface Product {
-    double price = 0;
-    String name = null;
-    Date MFG = null;//manufacturing date
-    Date EXP = null;//Expiration date
+    //double price = 0;
+    //ProductName name = null;
+    //Date MFG = null;//manufacturing date
+    //Date EXP = null;//Expiration date
 
      double getPrice();
+     String getName();
+     Date getMFG();
+     Date getEXP();
     
 }
