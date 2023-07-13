9c88dc5d1ff7cba4b2b7e3a29595a603f93bf79e
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-07-13 21:09:58 +0330

    add & remove & update added

--- Interface/StoreRoom.java
+++ Interface/StoreRoom.java
@@ -1,5 +1,8 @@
 package Interface;
 
 public interface StoreRoom {
-    
+    Product search();
+    void add(Product product);
+    void remove(Product product);
+    void Update(Product product);
 }
