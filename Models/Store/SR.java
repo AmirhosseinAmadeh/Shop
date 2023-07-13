7ef6702e7cfccb49434f6f28a787b5c3e849e762
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-07-13 22:10:55 +0330

    Capacity added

--- Models/Store/SR.java
+++ Models/Store/SR.java
@@ -5,6 +5,15 @@ import Interface.StoreRoom;
 
 public class SR implements StoreRoom {
 SplayTree list;
+int Capacity;
+public SR(int capacity) {
+    this.list = new SplayTree();
+    Capacity = capacity;
+}
+public void setCapacity(int capacity) {
+    Capacity = capacity;
+}
+
     
 }
 
