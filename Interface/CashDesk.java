66ecd029e3284176bbb1300dad0ac6dcac053d9f
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-08-04 15:40:05 +0330

    start to add method to chashDesk

--- Interface/CashDesk.java
+++ Interface/CashDesk.java
@@ -1,5 +1,10 @@
 package Interface;
 
+import java.util.ArrayList;
+
 public interface CashDesk {
-    
+    void addtoShopingList(Product product);
+    void removefromShopingList(Product product);
+    ArrayList<Product> getShopingList();
+    void PurchaseConfirmation();
 }
