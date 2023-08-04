c622680993a2d3245cc7443d58158b67c69c92ad
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-08-04 17:46:33 +0330

    start to imolement CashDesk

--- Models/Store/CashDesk/CD.java
+++ Models/Store/CashDesk/CD.java
@@ -1,7 +1,40 @@
 package Models.Store.CashDesk;
 
+import java.util.ArrayList;
+
 import Interface.CashDesk;
+import Interface.Product;
 
 public class CD implements CashDesk {
+
+    @Override
+    public void addtoShopingList(Product product) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addtoShopingList'");
+    }
+
+    @Override
+    public void removefromShopingList(Product product) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removefromShopingList'");
+    }
+
+    @Override
+    public void Display() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Display'");
+    }
+
+    @Override
+    public ArrayList<Product> getShopingList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getShopingList'");
+    }
+
+    @Override
+    public void PurchaseConfirmation(ArrayList<Product> ShopingList) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'PurchaseConfirmation'");
+    }
     
 }
