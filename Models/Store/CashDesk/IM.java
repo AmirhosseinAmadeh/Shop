aef971742786c6fedc796f4294ef312c35ab4a84
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-08-05 17:16:29 +0330

    start to implement item

---
+++ Models/Store/CashDesk/IM.java
@@ -0,0 +1,19 @@
+package Models.Store.CashDesk;
+
+import Interface.Item;
+
+public class IM implements Item {
+
+    @Override
+    public String getname() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getname'");
+    }
+
+    @Override
+    public int getid() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getid'");
+    }
+    
+}
