c4bb9af5b2330c673a527749d69cc020ea8d5b13
AmirhosseinAmadeh <100357291+AmirhosseinAmadeh@users.noreply.github.com>
2023-08-05 17:16:00 +0330

    shoping list have list of items

--- Models/Store/CashDesk/SL.java
+++ Models/Store/CashDesk/SL.java
@@ -1,8 +1,12 @@
 package Models.Store.CashDesk;
 
+import java.util.ArrayList;
+
 import Interface.ShopingList;
 
 public class SL implements ShopingList {
+
+    ArrayList<IM> list = new ArrayList<IM>();
 
     @Override
     public void Display() {
